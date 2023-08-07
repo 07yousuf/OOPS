3799f523682c8fbf71ad12a8162d1991f9a8bbb7
yousuf <ysyousuf500@gmail.com>
2023-08-07 18:43:39 +0600

    initial

---
+++ Cpp/concept1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class sample {
+      public:
+      int x,y;
+      int sum(int x, int y){
+            int sum = x+y;
+            return sum;
+      }
+};
+int main(){
+      sample obj1;
+      cin>>obj1.x>>obj1.y;
+
+      cout<<obj1.x<<endl<<obj1.y<<endl<<obj1.sum(obj1.x,obj1.y);
+}
