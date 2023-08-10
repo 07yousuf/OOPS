e37dd7930cefdde67e8cf452681b93bf9326dfd3
yousuf <ysyousuf500@gmail.com>
2023-08-10 23:56:23 +0600

    initial

---
+++ Cpp/OverLoading5.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class overloading{
+      public:
+      int a;float b;
+      void overloading(int a, int b){
+            cout<<"Summation is:"<<a+b;
+      }
+};
+int main(){
+      overloading r1;
+      
+}
