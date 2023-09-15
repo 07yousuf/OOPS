2e1078c0ddbf9a048d7230bd98a0467e54809e11
yousuf <ysyousuf500@gmail.com>
2023-09-15 19:31:19 +0600

    initial

---
+++ Cpp/multilevelInheritance27.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+class animal{
+      public:
+      int weight;
+      string color;
+      void doginfo(int x,string y){
+            weight = x;
+            color = y;
+      }
+      void show(){
+            cout<<weight<<endl<<color<<endl;
+      }
+};
+
+class dog : public animal{
+      public:
+      string power;
+      void doginfo1(string x){
+            power=x;
+      }
+      string show1(){
+            return power;
+      }
+};
+
+class jermansheperd : public dog{
+      public:
+      string speed;
+      void doginfo2(string x){
+            speed = x;
+      }
+      string show2(){
+            return speed;
+      }
+};
+
+int main(){
+      jermansheperd object1;
+      object1.doginfo(40,"white");
+      object1.show();
+      object1.doginfo1("beast");
+      cout<<object1.show1()<<endl;
+      object1.doginfo2("40kmh");
+      cout<<object1.show2()<<endl;
+
+}
